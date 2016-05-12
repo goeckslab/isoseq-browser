e0c5a8fb6ce7af0cba8849a92dae47879ed6dbda
Jingyuan Hu <jingyuanhu0626@gmail.com>
2016-05-12 23:49:29 +0800

    add comments

--- browse.py
+++ browse.py
@@ -6,118 +6,129 @@ from getGene import *
 from bokeh.plotting import Figure
 from bokeh.models import ColumnDataSource, HoverTool, HBox, VBoxForm, PanTool, WheelZoomTool, BoxZoomTool, ResetTool, ResizeTool, PreviewSaveTool, Range1d, LinearAxis
 from bokeh.io import curdoc
-from bokeh.models.widgets import Slider, Select, TextInput, Button, VBox, PreText, DataTable, TableColumn, AutocompleteInput
+from bokeh.models.widgets import Slider, Select, TextInput, Button, VBox, PreText, DataTable, TableColumn
 from sklearn.cluster import KMeans
 import pandas as pd
 from collections import Counter
 
-PLOT_WIDTH = 1200
-TITLE_FONT_SIZE = "25pt"
-REFERENCE_COLOR = "#22313F"
-MATCH_COLOR = "#52B3D9"
+PLOT_WIDTH = 1200                                           # the width of plott
+TITLE_FONT_SIZE = "25pt"                                    # font size of plot title
+REFERENCE_COLOR = "#22313F"                                 # the color of Reference transcripts
+MATCH_COLOR = "#52B3D9"                                     # the color of matched isoforms
 
+# Select isoforms of a particular gene
 def selectGene(opt):
     tranList = list()                                      # list of Transcript objects
     exonList = list()                                      # list of Exon objects
-    global isAnnot, isMatch
+    global isAnnot, isMatch                                # whether annotation transcripts or matched isoforms are in the inputs
+
     if opt.gtf is not None:
         try:
-            getGeneFromAnnotation (opt, tranList, exonList)    # lists will be changed
+            getGeneFromAnnotation (opt, tranList, exonList)                 # find transcripts, exons of the gene in the reference database
             isAnnot = True
-        except (RuntimeError, IOError):
+        except RuntimeError:                                                # if gene is not in the annotation file
             Console.text = 'Console:\ngene %s is not in the \nannotation file' % opt.gene
+            isAnnot = False
+        except IOError:                                                     # if annotation file is not found
+            Console.text = 'Console:\nannotations file %s is not found' % opt.gtf
             isAnnot = False
     if opt.matches is not None:
         try:
             Console.text = 'Console:\nReading pickle file...'
-            getGeneFromMatches (opt, tranList, exonList)       # lists will be changed
+            getGeneFromMatches (opt, tranList, exonList)                    # find transcripts, exons of the gene in the matched isoforms
             isMatch = True
-        except (RuntimeError, IOError):
+        except RuntimeError:                                                # if gene is not in the matched file
             Console.text = 'Console:\ngene %s is not in the \nmatch file' % opt.gene
             isMatch = False
-    if len(exonList) == 0:
+        except IOError:                                                     # if mached file is not found
+            Console.text = 'Console:\nannotations file %s is not found' % opt.matches
+            isMatch = False
+
+    if len(exonList) == 0:                                                  # if there is no exons in the annotation or match files
         Console.text = 'Console:\nno exons found for gene %s \nin annotation or match files' % opt.gene
         raise RuntimeError ('Console:\nno exons found for gene %s \nin annotation or match files' % opt.gene)
         return
     return tranList, exonList
 
-def getLength(row):
-    return len(row['Clusters'])
-
-def getChromosome(tranList):
+def getChromosome(tranList):                                # find out which chromosome does the gene locate on
+    chromosome = None
     for tran in tranList:
         if tran.annot is False:
             chromosome = tran.chr
             break
     return chromosome
 
-def updateMatch():
-    global matchList
+def updateMatch():                                      # update visuzlization  if the match files changes, return True if update is needed
+    checkUpdate = Matches.value.strip().split(',')
+    checkUpdate = ['../' + x for x in checkUpdate]
     try:
-        if matchList == Matches.value.strip().split(','):
+        if matchList == checkUpdate:
             return False
         else:
-            matchList = Matches.value.strip().split(',')
             return True
     except NameError:
-        matchList = Matches.value.strip().split(',')
         return True
 
-def updateGene(attrname, old, new):
-    source.data=dict(x=[], y=[], color=[], line_alpha=[],
+def updateGene(attrname, old, new):                     # update visualization if the gene changes
+    source.data=dict(x=[], y=[], color=[], line_alpha=[],                       # the data source for plotting transcripts
                                  QScore=[], start=[], end=[])
-    blockSource.data=dict(top=[], bottom=[], left=[], right=[], exon=[],
+    blockSource.data=dict(top=[], bottom=[], left=[], right=[], exon=[],        # the data source for plotting block boundary
                         start=[], end=[], chromosome=[], xs=[], ys=[])
-    p.title = ""
+    p.title = "Transcript of %s" % Gene.value.strip()                           # update the title of plot
 
-    allGenes = Counter()
-    if updateMatch():
-        matchList == Matches.value.strip().split(',')
-        for matchFile in matchList:
-            geneDict = dict()
+    # create and display a list of all the genes
+    global matchList
+    if updateMatch():                                               # if matches files are updated
+        matchList = Matches.value.strip().split(',')
+        allGenes = Counter()                                        # create a counter hastable(dictionary) object
+        for matchFile in matchList:                                 # there can be multiple match files as inputs
+            print '----------------------------------------------'
+            print matchFile
             try:
                 clusterDict = cl.ClusterDict.fromPickle(matchFile)            # pickle file produced by matchAnnot.py
+                print clusterDict
             except IOError:
                 Console.text = 'Console:\nNo such file: %s\n change gene to restart' %matchFile
                 break
+            geneDict = dict()                                               # how many isoforms for each gene
             myDict = clusterDict.getGeneDict()
             for key, val in myDict.iteritems():
                 geneDict.setdefault(key, len(val))
             geneDict = Counter(geneDict)
-            allGenes = allGenes + geneDict
-        Auto.completions = list(allGenes.keys())
-        geneSource.data = dict(
+            allGenes = allGenes + geneDict                                  # add information of new match file
+        geneSource.data = dict(                                             # update the table in visualization
             Gene = allGenes.keys(),
-            Cluster = allGenes.values(),
+            Isoforms = allGenes.values(),
         )
 
+    # load the reference transcripts
     global Annotations, gtf, opt
     Console.text = 'Console:\nReading annotation file...'
-    try:
+    try:                                                                    # if it's not the first time make the plot
         Annotations, gtf
-        if gtf != GTF.value.strip():
+        if gtf != GTF.value.strip():                                        # and if the annotation file changes
             try:
-                Annotations = getAnnotations(getParams(GTF.value.strip(), None, None))
-            except IOError:
+                Annotations = getAnnotations(getParams(GTF.value.strip(),   # get the lists of transcripts in annot file
+                                None, None))                                # hold it in RAM for quicker respond when changing the gene
+            except IOError:                                               # annotation file not found
                 Console.text = 'Console:\nNo such file: %s' %str(gtf)
                 Annotations = None
             gtf = GTF.value.strip()
-    except NameError:
+    except NameError:                                   # if it's the first time make the plot, do the same
         gtf = GTF.value.strip()
-        print gtf
         try:
             Annotations = getAnnotations(getParams(gtf, None, None))
         except IOError:
             Console.text = 'Console:\nNo such file: %s' %str(gtf)
             Annotations = None
 
-    opt = getParams(GTF.value.strip(), matchList,
+    opt = getParams('../' + GTF.value.strip(), matchList,               # get the options from inputs
             Gene.value.strip(), forMat=Format.value.strip(), annotations=Annotations)
     opt.gene = Gene.value.strip()
 
     global tranList
-    tranList, exonList = selectGene(opt)
-    chromosome = getChromosome(tranList)
+    tranList, exonList = selectGene(opt)                    # select transcripts by gene
+    chromosome = getChromosome(tranList)                    # find out which chromosome does the gene locate
 
     forwardStrand = '-' if opt.flip else '+'
     if exonList[0].strand == forwardStrand:
@@ -128,34 +139,26 @@ def updateGene(attrname, old, new):
         blocks = assignBlocksReverse (opt, exonList)       # assign each exon to a block -- backwards
 
     findRegions (tranList)                       # determine regions occupied by each transcript
-    tranNames = orderTranscripts (tranList)
-    tranNames = changeNames(tranNames)
-    global length, df, colorDF,boundaryDF, height
+    tranNames = orderTranscripts (tranList)                 # get the names of transcripts, placed them in the right order
+    tranNames = changeNames(tranNames)                      # if the length of name is too long, reduce it
+
+    global length, df, colorDF,boundaryDF
     length = len(tranNames)
     Console.text = 'Console:\nCreating plot...'
 
-    maxVal = 0
-    minVal = float('inf')
-    for tran in tranList:
-        if tran.annot == False:
-            if maxVal < tran.end:
-                maxVal = tran.end
-            if minVal > tran.start:
-                minVal = tran.start
+    p.plot_height = Width.value*2*(length+4)                   # set the height of plot according to the length of transcripts
 
-    p.plot_height = 40*(length+4)
-    p.title = "Transcript of %s" % opt.gene
-    p.y_range.factors = tranNames[::-1]
+    p.y_range.factors = tranNames[::-1]             # set the y axis tick to the transcripts names
 
     Console.text = 'grouping...'
     if Group.value == "on" and isMatch is True:
-        colorDF = groupTran(tranList, exonList, 5)
+        colorDF = groupTran(tranList, exonList, 5)          # group the transcripts by similarities
     else:
         colorDF = None
 
-    getExonData(exonList, colorDF)
+    getExonData(exonList, colorDF)                          # get the data of each isoform that can be directly used to plot, put it into a global dataframe
 
-    source.data = dict(
+    source.data = dict(                                     # update the data for plot
         xs=df['xs'],
         ys=df['ys'],
         color=df['colors'],
@@ -167,7 +170,7 @@ def updateGene(attrname, old, new):
         end=df['end'],
         width = df['width'],
     )
-    getBoundaryData(blocks)
+    getBoundaryData(blocks)                                 # get the data of each block that can be directly used to plot, put it into a global dataframe
 
     xs = list(boundaryDF['xs'])
     ys= list(boundaryDF['ys'])
@@ -213,6 +216,7 @@ def updateFP(attrname, old, new):
         width=df['width'],
     )
 
+# update the number of groups
 def updateGroup(attrname, old, new):
     colors = list()
     global colorDF, df
@@ -233,6 +237,7 @@ def updateGroup(attrname, old, new):
         width=df['width'],
     )
 
+# update the width of the each isoform
 def updateWidth(attrname, old, new):
     df['width'] = Width.value
     p.plot_height = Width.value*2*(length+4)
@@ -248,7 +253,6 @@ def updateWidth(attrname, old, new):
         end=df['end'],
         width=df['width'],
     )
-
 
 def getExonData(exonList, colorDF):
     global df
@@ -277,7 +281,7 @@ def getExonData(exonList, colorDF):
 
         df = df.append(data,ignore_index=True)
     df['alpha'] = 1
-    df['width'] = 20
+    df['width'] = Width.value
 
 def getColor(exonName, colorDF):
     if exonName not in list(colorDF.name):
@@ -293,6 +297,7 @@ def getColor(exonName, colorDF):
             color = row['color1']
     return color
 
+# find out the position of boundaries
 def getBoundaryData(blocks):
     boundaryDF = pd.DataFrame()
     boundary = list()
@@ -302,6 +307,7 @@ def getBoundaryData(blocks):
         boundary.append(bound.boundary)
         start.append(bound.start)
         end.append(bound.end)
+    # add data used for plotting to pandas DataFrame
     global boundaryDF
     boundaryDF['boundary'] = boundary
     boundaryDF['xs'] = zip(boundary, boundary)
@@ -310,6 +316,7 @@ def getBoundaryData(blocks):
     boundaryDF['end'] = end
     boundaryDF = boundaryDF.sort('boundary')
 
+# find out which isoform is below full/partial threshold, which isoform is not
 def greaterFP(row):
     alphaVal = Alpha.value
     if row['annot'] is False:
@@ -318,6 +325,7 @@ def greaterFP(row):
     else:
         return 1.0
 
+# save the transcripts to .fasta file, the function is copied from MatchAnnot
 def saveFasta(attrname, old, new):
     opt.fasta = Save.value.strip()
     tranList = list()
@@ -325,6 +333,7 @@ def saveFasta(attrname, old, new):
     getGeneFromMatches (opt, tranList, exonList)
     opt.fasta = None
 
+# create the visualization plot
 def createPlot(df, boundaryDF):
     p = Figure(plot_height=900, plot_width=PLOT_WIDTH, title="", y_range=[],
                 title_text_font_size=TITLE_FONT_SIZE)
@@ -344,6 +353,7 @@ def createPlot(df, boundaryDF):
                 ("start", "@start"), ("end", "@end")], renderers=[quad]))
     return p
 
+# a class containing all the input parameters
 class getParams(object):
     def __init__(self, gtf, matches, gene, forMat="standard", omit=None,
                  show=None, howmany=None, nodups=None, minlen=None, maxlen=None, output="exon.png",
@@ -371,9 +381,10 @@ class getParams(object):
         self.partial = partial
         self.annotations = annotations
 
-GTF = TextInput(title="Enter the name of annotation file", value="gencode.vM8.annotation.gtf")
+# create all kinds of widgets
+GTF = TextInput(title="Enter the name of annotation file", value="gencode.vM9.annotation.gtf")
 Format = TextInput(title="Enter the format of annotation file, standard is gtf", value="standard")
-Matches = TextInput(title="Enter the name of pickle files from MatchAnnot, e.g. of multiple files: match1.pickle,match2.pickle", value="matches.pickle")
+Matches = TextInput(title="Enter the name of pickle files from MatchAnnot, e.g. of multiple files: a.pickle,b.pickle", value="matches.pickle")
 Gene = TextInput(title="Select gene to visualize")
 Alpha = Slider(title="Alpha value of exons", value=1.0, start=0, end=1.0, step=0.1)
 Full = Slider(title="Full support threshold", value=0, start=0, end=30, step=1.0)
@@ -382,38 +393,28 @@ Group = Select(title="Group isoform or not", value="on", options=["on", "off"])
 Cluster = Slider(title="The number of groups", value=3, start=1, end=5, step=1.0)
 Width = Slider(title="The width of transcripts", value=20, start=5, end=30, step=1)
 Save = TextInput(title="Enter the folder name to data in Fasta", value=None)
-Auto = AutocompleteInput(title="AutoComplete", completions=[])
 
+# the data used for plotting isoforms, boundaries and gene
 blockSource = ColumnDataSource(data=dict(top=[], bottom=[], left=[], right=[], exon=[],
                                 start=[], end=[], chromosome=[], xs=[], ys=[]))
 source = ColumnDataSource(data=dict(x=[], y=[], color=[], line_alpha=[], width=[],
                             QScore=[], start=[], end=[]))
-geneSource = ColumnDataSource(data=dict(Gene=[], Cluster=[]))
+geneSource = ColumnDataSource(data=dict(Gene=[], Isoforms=[]))
 
-df = pd.DataFrame()
-boundaryDF = pd.DataFrame()
-colorDF = pd.DataFrame()
-outDF = pd.DataFrame()
-
-
+# the console box
 Console = PreText(text='Console:\nStart visualize by entering \nannotations, pickle file and gene.\nPress Enter to submit.\n',
                     width=250, height=100)
-p = createPlot(df, boundaryDF)
+# the visualization plot
+p = createPlot(pd.DataFrame(), pd.DataFrame())
 
-Gene.on_change('value', updateGene)
-Full.on_change('value', updateFP)
-Partial.on_change('value', updateFP)
-Alpha.on_change('value', updateFP)
-Cluster.on_change('value', updateGroup)
-Save.on_change('value', saveFasta)
-Width.on_change('value', updateWidth)
-
-dataColumns = [
+# a table of with all the genes in the match files, and how many isoforms in each gene
+geneColumns = [
         TableColumn(field="Gene", title="Gene"),
-        TableColumn(field="Cluster", title="Cluster"),
+        TableColumn(field="Isoforms", title="Isoforms"),
     ]
-data_table = DataTable(source=geneSource, columns=dataColumns, width=200, height=1200)
+geneCountTable = DataTable(source=geneSource, columns=geneColumns, width=200, height=1200)
 
+# a description of all the parameters
 paramSource = ColumnDataSource(data=dict(
         Parameter=['annotation', 'format', 'matches', 'gene', 'full', 'alpha', 'partial', 'group', '# of groups', 'fasta'],
         Description=['annotations file, in format specified by --format, Reload page to update' ,
@@ -432,9 +433,19 @@ paramColumns = [
         TableColumn(field="Parameter", title="Parameter"),
         TableColumn(field="Description", title="Description"),
     ]
-param_table = DataTable(source=paramSource, columns=paramColumns, width=1200, height=700)
-main = VBoxForm(p, param_table)
+paramTable = DataTable(source=paramSource, columns=paramColumns, width=1200, height=700)
 
-controls = [Console, GTF, Format, Matches, Auto, Gene, Width, Alpha, Full, Partial, Group, Cluster, Save]
+# make changes to the plot when widgets are updated
+Gene.on_change('value', updateGene)
+Full.on_change('value', updateFP)
+Partial.on_change('value', updateFP)
+Alpha.on_change('value', updateFP)
+Cluster.on_change('value', updateGroup)
+Save.on_change('value', saveFasta)
+Width.on_change('value', updateWidth)
+
+# the position of plot and widgets
+controls = [Console, GTF, Format, Matches, Gene, Width, Alpha, Full, Partial, Group, Cluster, Save]
+main = VBoxForm(p, paramTable)
 inputs = HBox(VBoxForm(*controls), width=250)
-curdoc().add_root(HBox(inputs, main, data_table, width=1800))
+curdoc().add_root(HBox(inputs, main, geneCountTable, width=1800))
